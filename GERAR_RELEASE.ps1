2d6cade905f3ad21ff09bda7eca77c666a99c4f4
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-07 16:50:59 -0300

    chore(release): simplificar comando local de release

--- GERAR_RELEASE.ps1
+++ GERAR_RELEASE.ps1
@@ -1,16 +1,6 @@
-param(
-  [Parameter(Mandatory=$true)][string]$PrivateKey,
-  [string]$BaseUrl="",
-  [ValidateSet("stable","beta")][string]$Channel="stable",
-  [string]$RollbackInstaller="",
-  [string]$RollbackUrl="",
-  [switch]$Mandatory
-)
 $ErrorActionPreference="Stop"
+
 python -m pip install -r requirements-dev.txt
-$args=@("tools/build_release.py","--private-key",$PrivateKey,"--channel",$Channel)
-if($BaseUrl){$args += @("--base-url",$BaseUrl)}
-if($RollbackInstaller){$args += @("--rollback-installer",$RollbackInstaller)}
-if($RollbackUrl){$args += @("--rollback-url",$RollbackUrl)}
-if($Mandatory){$args += "--mandatory"}
-python @args
+python -m pip install -r license_server/requirements.txt
+python -m pip install httpx2
+python tools/build_release.py
